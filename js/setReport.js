a6c3b5b9fdabc52bad292cfe2093d35cb2808ad3
Tiago Gimenes Scarpelin <110875258+TiagoScarpelin@users.noreply.github.com>
2024-02-04 01:31:49 -0300

    Delete js directory

--- js/setReport.js
+++
@@ -1,8 +0,0 @@
-function BuscaData(){
-
-    let data_relatorio = document.getElementById("data").value;
-    $.post("php/processa.php", {act: "relatorio_mensal",data_relatorio: data_relatorio}, function(result)
-    {
-       
-    });
-}
